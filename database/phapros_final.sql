bc0b77b3a11d7f0c937bcdf22e4884ed6847012a
niko2296 <niko.kusdiarto@gmail.com>
2019-03-25 11:01:53 +0700

    Mengcopykan database terbaru

--- database/phapros_final.sql
+++ database/phapros_final.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 15, 2019 at 11:03 PM
+-- Generation Time: Mar 25, 2019 at 05:00 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.12
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `phapros`
+-- Database: `phapros_final`
 --
 
 -- --------------------------------------------------------
@@ -45,9 +45,8 @@ CREATE TABLE `akses_menu` (
 --
 
 INSERT INTO `akses_menu` (`id_akses`, `id_jabatan`, `menu1`, `menu2`, `menu3`, `menu4`, `menu5`, `menu6`, `menu7`) VALUES
-(1, 3, 1, 0, 1, 0, 1, 1, 1),
-(2, 2, 1, 0, 0, 0, 1, 1, 1),
-(3, 4, 0, 0, 0, 0, 1, 0, 0);
+(1, 3, 0, 0, 0, 0, 1, 0, 0),
+(2, 2, 0, 0, 0, 0, 1, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -68,10 +67,53 @@ CREATE TABLE `aturan_penilai` (
 --
 
 INSERT INTO `aturan_penilai` (`id_aturan`, `id_jabatan_penilai`, `id_unit_penilai`, `id_jabatan_dinilai`, `id_unit_dinilai`) VALUES
-(4, 3, 1, 4, 1),
-(5, 2, 1, 3, 1),
-(6, 2, 1, 4, 1),
-(11, 2, 2, 3, 2);
+(1, 2, 1, 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aturan_polarisasi`
+--
+
+CREATE TABLE `aturan_polarisasi` (
+  `id_aturan_polarisasi` bigint(20) NOT NULL,
+  `id_polarisasi` bigint(20) NOT NULL,
+  `bmi` decimal(10,2) NOT NULL,
+  `bma` decimal(10,2) NOT NULL,
+  `poin` decimal(10,2) NOT NULL,
+  `tanggal_input` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `aturan_polarisasi`
+--
+
+INSERT INTO `aturan_polarisasi` (`id_aturan_polarisasi`, `id_polarisasi`, `bmi`, `bma`, `poin`, `tanggal_input`) VALUES
+(8, 10, '1.00', '20.00', '1.00', '2019-03-21'),
+(14, 23, '1.00', '20.00', '1.00', '2019-03-21'),
+(15, 23, '21.00', '40.00', '2.00', '2019-03-21'),
+(18, 2, '1.00', '50.00', '1.00', '2019-03-21'),
+(21, 4, '1.00', '20.00', '1.00', '2019-03-21'),
+(22, 4, '21.00', '40.00', '2.00', '2019-03-21'),
+(23, 5, '1.00', '50.00', '1.00', '2019-03-21'),
+(24, 6, '1.00', '20.00', '1.00', '2019-03-21'),
+(25, 6, '21.00', '40.00', '2.00', '2019-03-21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_catatan`
+--
+
+CREATE TABLE `data_catatan` (
+  `id_catatan` bigint(20) NOT NULL,
+  `id_anggota` bigint(20) NOT NULL,
+  `id_jabatan` bigint(20) NOT NULL,
+  `id_unit` bigint(20) NOT NULL,
+  `id_periode` bigint(20) NOT NULL,
+  `catatan` text NOT NULL,
+  `tanggal_input` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -84,7 +126,7 @@ CREATE TABLE `data_kpi` (
   `id_anggota` bigint(20) NOT NULL DEFAULT '0',
   `id_jabatan` bigint(20) NOT NULL DEFAULT '0',
   `id_unit` bigint(20) DEFAULT '0',
-  `tahun` int(20) NOT NULL DEFAULT '0',
+  `id_periode` bigint(20) NOT NULL DEFAULT '0',
   `kpi` text NOT NULL,
   `deskripsi` text NOT NULL,
   `bobot` decimal(10,0) NOT NULL DEFAULT '0',
@@ -101,15 +143,9 @@ CREATE TABLE `data_kpi` (
 -- Dumping data for table `data_kpi`
 --
 
-INSERT INTO `data_kpi` (`id_kpi`, `id_anggota`, `id_jabatan`, `id_unit`, `tahun`, `kpi`, `deskripsi`, `bobot`, `sasaran`, `satuan`, `sifat_kpi`, `status`, `id_verifikator`, `tanggal_input`, `tanggal_verifikasi`) VALUES
-(1, 2, 4, 1, 2019, 'Coba 1', 'Ini Uji Coba 1', '30', '10', '1', 1, 1, 0, '2019-03-11', '2019-03-14'),
-(2, 2, 4, 1, 2019, 'Coba 2', 'Ini Uji Coba 2', '70', '20', '3', 5, 1, 1, '2019-03-11', '2019-03-12'),
-(3, 1, 3, 1, 2019, 'Baru Coba 1', 'Ini Baru Coba 1', '20', '5', '1', 1, 1, 0, '2019-03-12', '2019-03-14'),
-(5, 1, 3, 1, 2019, 'Baru Coba 2', 'Ini Baru Coba 2', '80', '10', '3', 5, 1, 0, '2019-03-13', '2019-03-14'),
-(7, 2, 4, 1, 2010, 'Baru Sekali 1', 'Ini Baru Sekali 1', '60', '30', '1', 2, 1, 1, '2019-03-14', '2019-03-14'),
-(8, 2, 4, 1, 2010, 'Baru Sekali 2', 'Baru Sekali 2', '40', '15', '3', 5, 1, 1, '2019-03-14', '2019-03-14'),
-(9, 1, 3, 1, 2021, 'Baru Coba 1', 'Ini Baru Coba 1', '20', '5', '1', 1, 1, 0, '2019-03-15', '2019-03-15'),
-(10, 1, 3, 1, 2021, 'Baru Coba 2', 'Ini Baru Coba 2', '80', '10', '3', 5, 1, 0, '2019-03-15', '2019-03-15');
+INSERT INTO `data_kpi` (`id_kpi`, `id_anggota`, `id_jabatan`, `id_unit`, `id_periode`, `kpi`, `deskripsi`, `bobot`, `sasaran`, `satuan`, `sifat_kpi`, `status`, `id_verifikator`, `tanggal_input`, `tanggal_verifikasi`) VALUES
+(2, 1, 3, 1, 3, 'Coba Edit 2', 'Ini Coba Edit 2', '80', '30', '9', 5, 0, 2, '2019-03-22', '0000-00-00'),
+(3, 1, 3, 1, 3, 'Coba Edit 1', 'Ini Coba Edit 1', '10', '3', '10', 6, 0, 2, '2019-03-22', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -139,9 +175,8 @@ CREATE TABLE `mst_anggota` (
 --
 
 INSERT INTO `mst_anggota` (`id_anggota`, `nik`, `nama`, `jenis_kelamin`, `tempat_lahir`, `tanggal_lahir`, `status`, `nomor_hp`, `email`, `id_golongan`, `id_jabatan`, `id_unit`, `alamat`, `tanggal_input`) VALUES
-(1, '11111', 'Eko Sudiarto', 1, 'Tegal', '1966-06-22', 2, '085865244793', 'ekosudiarto@gmail.com', 1, 3, 1, 'Ungaran', '2019-03-11'),
-(2, '22222', 'Niko Kusdiarto', 1, 'Kabupaten Semarang', '1996-02-02', 1, '082118009419', 'niko.kusdiarto@gmail.com', 3, 4, 1, 'Leyangan', '2019-03-11'),
-(3, '33333', 'Kusneni Rositawati', 2, 'Semarang', '1965-06-30', 2, '081575662267', 'kusneni@gmail.com', 4, 2, 1, 'Leyangan', '2019-03-13');
+(1, '11111', 'Niko Kusdiarto', 1, 'Kabupaten Semarang', '1996-02-02', 1, '082118009419', 'niko.kusdiarto@gmail.com', 1, 3, 1, 'Ungaran', '2019-03-21'),
+(2, '22222', 'Eko Sudiarto', 1, 'Tegal', '1966-06-22', 2, '085865244793', 'ekosud@gmail.com', 1, 2, 1, 'Leyangan', '2019-03-21');
 
 -- --------------------------------------------------------
 
@@ -160,10 +195,7 @@ CREATE TABLE `mst_golongan` (
 --
 
 INSERT INTO `mst_golongan` (`id_golongan`, `nama_golongan`, `tanggal_input`) VALUES
-(1, 'I A', '2019-03-11'),
-(2, 'I B', '2019-03-11'),
-(3, 'II A', '2019-03-11'),
-(4, 'II B', '2019-03-11');
+(1, 'I A', '2019-03-21');
 
 -- --------------------------------------------------------
 
@@ -174,7 +206,6 @@ INSERT INTO `mst_golongan` (`id_golongan`, `nama_golongan`, `tanggal_input`) VAL
 CREATE TABLE `mst_jabatan` (
   `id_jabatan` bigint(20) NOT NULL,
   `nama_jabatan` varchar(50) NOT NULL,
-  `akses_nilai` int(2) NOT NULL DEFAULT '0',
   `tanggal_input` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -182,11 +213,52 @@ CREATE TABLE `mst_jabatan` (
 -- Dumping data for table `mst_jabatan`
 --
 
-INSERT INTO `mst_jabatan` (`id_jabatan`, `nama_jabatan`, `akses_nilai`, `tanggal_input`) VALUES
-(1, 'Direktur', 1, '2019-03-11'),
-(2, 'Manager', 1, '2019-03-11'),
-(3, 'Asisten Manager', 1, '2019-03-11'),
-(4, 'Programmer', 0, '2019-03-11');
+INSERT INTO `mst_jabatan` (`id_jabatan`, `nama_jabatan`, `tanggal_input`) VALUES
+(2, 'Asisten Manager', '2019-03-21'),
+(3, 'Programmer', '2019-03-21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mst_periode`
+--
+
+CREATE TABLE `mst_periode` (
+  `id_periode` bigint(20) NOT NULL,
+  `tahun` int(10) NOT NULL,
+  `status` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mst_periode`
+--
+
+INSERT INTO `mst_periode` (`id_periode`, `tahun`, `status`) VALUES
+(2, 2019, 0),
+(3, 2020, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mst_polarisasi`
+--
+
+CREATE TABLE `mst_polarisasi` (
+  `id_polarisasi` bigint(20) NOT NULL,
+  `nama_polarisasi` varchar(50) NOT NULL,
+  `id_periode` bigint(20) NOT NULL,
+  `tanggal_input` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mst_polarisasi`
+--
+
+INSERT INTO `mst_polarisasi` (`id_polarisasi`, `nama_polarisasi`, `id_periode`, `tanggal_input`) VALUES
+(2, 'Negatif', 2, '2019-03-21'),
+(4, 'Absolute Positif', 2, '2019-03-21'),
+(5, 'Negatif', 3, '2019-03-21'),
+(6, 'Absolute Positif', 3, '2019-03-21');
 
 -- --------------------------------------------------------
 
@@ -198,6 +270,7 @@ CREATE TABLE `mst_satuan` (
   `id_satuan` bigint(20) NOT NULL,
   `nama_satuan` varchar(50) NOT NULL,
   `jenis_polarisasi` text NOT NULL,
+  `id_periode` bigint(20) NOT NULL,
   `tanggal_input` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -205,9 +278,11 @@ CREATE TABLE `mst_satuan` (
 -- Dumping data for table `mst_satuan`
 --
 
-INSERT INTO `mst_satuan` (`id_satuan`, `nama_satuan`, `jenis_polarisasi`, `tanggal_input`) VALUES
-(1, '%', 'a:6:{i:0;s:1:\"1\";i:1;s:1:\"2\";i:2;s:1:\"3\";i:3;s:1:\"4\";i:4;s:1:\"5\";i:5;s:1:\"6\";}', '2019-03-12'),
-(3, 'Jam', 'a:1:{i:0;s:1:\"5\";}', '2019-03-11');
+INSERT INTO `mst_satuan` (`id_satuan`, `nama_satuan`, `jenis_polarisasi`, `id_periode`, `tanggal_input`) VALUES
+(1, '%', 'a:1:{i:0;s:1:\"2\";}', 2, '2019-03-21'),
+(2, 'Coba 1', 'a:2:{i:0;s:1:\"2\";i:1;s:1:\"4\";}', 2, '2019-03-21'),
+(9, '%', 'a:1:{i:0;s:1:\"5\";}', 3, '2019-03-21'),
+(10, 'Coba 1', 'a:2:{i:0;s:1:\"5\";i:1;s:1:\"6\";}', 3, '2019-03-21');
 
 -- --------------------------------------------------------
 
@@ -226,8 +301,7 @@ CREATE TABLE `mst_unit` (
 --
 
 INSERT INTO `mst_unit` (`id_unit`, `nama_unit`, `tanggal_input`) VALUES
-(1, 'Teknologi Informasi', '2019-03-11'),
-(2, 'SDM', '2019-03-15');
+(1, 'Teknologi Informasi', '2019-03-21');
 
 -- --------------------------------------------------------
 
@@ -248,9 +322,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `username`, `password`, `status`, `aksus`) VALUES
-(1, '11111', 'e10adc3949ba59abbe56e057f20f883e', 1, 0),
-(2, '22222', '3d2172418ce305c7d16d4b05597c6a59', 1, 0),
-(3, '33333', 'b7bc2a2f5bb6d521e64c8974c143e9a0', 1, 0);
+(1, '11111', 'b0baee9d279d34fa1dfd71aadb908c3f', 1, 0),
+(2, '22222', '3d2172418ce305c7d16d4b05597c6a59', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -270,7 +343,7 @@ CREATE TABLE `waktu_input` (
 --
 
 INSERT INTO `waktu_input` (`id_waktu_input`, `tanggal_awal_input`, `tanggal_akhir_input`, `tanggal_input`) VALUES
-(2, '2019-03-01', '2019-03-31', '2019-03-15');
+(1, '2019-03-01', '2019-03-31', '2019-03-21');
 
 -- --------------------------------------------------------
 
@@ -290,7 +363,7 @@ CREATE TABLE `waktu_verifikasi` (
 --
 
 INSERT INTO `waktu_verifikasi` (`id_waktu_verifikasi`, `tanggal_awal_verifikasi`, `tanggal_akhir_verifikasi`, `tanggal_input`) VALUES
-(2, '2019-03-01', '2019-03-31', '2019-03-15');
+(1, '2019-03-01', '2019-03-31', '2019-03-21');
 
 --
 -- Indexes for dumped tables
@@ -307,6 +380,18 @@ ALTER TABLE `akses_menu`
 --
 ALTER TABLE `aturan_penilai`
   ADD PRIMARY KEY (`id_aturan`);
+
+--
+-- Indexes for table `aturan_polarisasi`
+--
+ALTER TABLE `aturan_polarisasi`
+  ADD PRIMARY KEY (`id_aturan_polarisasi`);
+
+--
+-- Indexes for table `data_catatan`
+--
+ALTER TABLE `data_catatan`
+  ADD PRIMARY KEY (`id_catatan`);
 
 --
 -- Indexes for table `data_kpi`
@@ -331,6 +416,18 @@ ALTER TABLE `mst_golongan`
 --
 ALTER TABLE `mst_jabatan`
   ADD PRIMARY KEY (`id_jabatan`);
+
+--
+-- Indexes for table `mst_periode`
+--
+ALTER TABLE `mst_periode`
+  ADD PRIMARY KEY (`id_periode`);
+
+--
+-- Indexes for table `mst_polarisasi`
+--
+ALTER TABLE `mst_polarisasi`
+  ADD PRIMARY KEY (`id_polarisasi`);
 
 --
 -- Indexes for table `mst_satuan`
@@ -370,67 +467,91 @@ ALTER TABLE `waktu_verifikasi`
 -- AUTO_INCREMENT for table `akses_menu`
 --
 ALTER TABLE `akses_menu`
-  MODIFY `id_akses` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_akses` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `aturan_penilai`
 --
 ALTER TABLE `aturan_penilai`
-  MODIFY `id_aturan` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_aturan` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `aturan_polarisasi`
+--
+ALTER TABLE `aturan_polarisasi`
+  MODIFY `id_aturan_polarisasi` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `data_catatan`
+--
+ALTER TABLE `data_catatan`
+  MODIFY `id_catatan` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `data_kpi`
 --
 ALTER TABLE `data_kpi`
-  MODIFY `id_kpi` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_kpi` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `mst_anggota`
 --
 ALTER TABLE `mst_anggota`
-  MODIFY `id_anggota` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_anggota` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `mst_golongan`
 --
 ALTER TABLE `mst_golongan`
-  MODIFY `id_golongan` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_golongan` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `mst_jabatan`
 --
 ALTER TABLE `mst_jabatan`
-  MODIFY `id_jabatan` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_jabatan` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `mst_periode`
+--
+ALTER TABLE `mst_periode`
+  MODIFY `id_periode` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `mst_polarisasi`
+--
+ALTER TABLE `mst_polarisasi`
+  MODIFY `id_polarisasi` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `mst_satuan`
 --
 ALTER TABLE `mst_satuan`
-  MODIFY `id_satuan` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_satuan` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `mst_unit`
 --
 ALTER TABLE `mst_unit`
-  MODIFY `id_unit` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_unit` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_user` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `waktu_input`
 --
 ALTER TABLE `waktu_input`
-  MODIFY `id_waktu_input` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_waktu_input` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `waktu_verifikasi`
 --
 ALTER TABLE `waktu_verifikasi`
-  MODIFY `id_waktu_verifikasi` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_waktu_verifikasi` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
